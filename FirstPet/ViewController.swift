9ca925124a1d9fa9dfbb760586cf794349bac9c2
Ulugbek <UlaXayt95gmail.com>
2024-08-01 15:45:57 +0300

    initial commit

--- FirstPet/ViewController.swift
+++ FirstPet/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var weather: UILabel!
+    @IBOutlet var button: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
     }
-
-
+    @objc func buttonAction(){
+        let urlString = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true"
+        let url = URL(string: urlString)!
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request){ data, response , error in
+            if let data, let weatherData = try? JSONDecoder().decode(WeatherData.self, from: data){
+                DispatchQueue.main.async {
+                    self.weather.text = "\(weatherData.currentWeather.temperature) °"
+                }
+            } else {
+                print("Fail!")
+            }
+        }
+        task.resume()
+    }
 }
 
---
+++ WeatherData.swift
@@ -0,0 +1,33 @@
+//
+//  WeatherData.swift
+//  FirstPet
+//
+//  Created by Улугбек Хайтметов on 01.08.2024.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let latitude, longitude, generationtimeMS: Double
+    let utcOffsetSeconds: Int
+    let timezone, timezoneAbbreviation: String
+    let elevation: Int
+    let currentWeather: CurrentWeather
+
+    enum CodingKeys: String, CodingKey {
+        case latitude, longitude
+        case generationtimeMS = "generationtime_ms"
+        case utcOffsetSeconds = "utc_offset_seconds"
+        case timezone
+        case timezoneAbbreviation = "timezone_abbreviation"
+        case elevation
+        case currentWeather = "current_weather"
+    }
+}
+
+// MARK: - CurrentWeather
+struct CurrentWeather: Codable {
+    let temperature, windspeed: Double
+    let winddirection, weathercode: Int
+    let time: String
+}
